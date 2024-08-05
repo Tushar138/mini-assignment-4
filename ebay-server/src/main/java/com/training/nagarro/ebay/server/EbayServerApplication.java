55b3bc21baf883b4e04a2ae92e0d1525d0a249cf
tushar01 <tushar01@nagarro.com>
2024-08-05 18:00:18 +0530

    Initial commit with project structure

---
+++ ebay-server/src/main/java/com/training/nagarro/ebay/server/EbayServerApplication.java
@@ -0,0 +1,13 @@
+package com.training.nagarro.ebay.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class EbayServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EbayServerApplication.class, args);
+	}
+}
---
+++ walmart-server/src/main/java/com/training/nagarro/walmart/server/WalmartServerApplication.java
@@ -0,0 +1,13 @@
+package com.training.nagarro.walmart.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class WalmartServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(WalmartServerApplication.class, args);
+	}
+}
